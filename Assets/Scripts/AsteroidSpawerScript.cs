23c634cbf8535f0888d190f841f9c72c9a4c0ae3
esalavat <eric.salavat@gmail.com>
2023-04-19 23:20:55 -0700

    Added first enemy ship

--- Assets/Scripts/AsteroidSpawerScript.cs
+++ Assets/Scripts/AsteroidSpawerScript.cs
@@ -6,18 +6,21 @@ using UnityEngine;
 public class AsteroidSpawerScript : MonoBehaviour
 {
     public GameObject[] asteroids;
+    public GameObject[] enemies;
     public float horizontalOffset = 10;
     public float spawnSpeed = 2;
     public float minSpawnSpeed = 0.5f;
     public float spawnRandomness = 1;
     private float timer = 0;
     private float nextSpawnTime;
+    private float nextEnemySpawnTime;
     private float spawnSpeedAcceleration = .001f;
 
     void Start()
     {
         spawnAsteroid();
-        nextSpawnTime = getNextSpawnTime();
+        nextSpawnTime = getNextAsteroidSpawnTime();
+        nextEnemySpawnTime = 20;
     }
 
     // Update is called once per frame
@@ -25,11 +28,15 @@ public class AsteroidSpawerScript : MonoBehaviour
     {
         if(timer > nextSpawnTime) {
             spawnAsteroid();
-            timer = 0;
-            nextSpawnTime = getNextSpawnTime();
-        } else {
-            timer+=Time.deltaTime;
+            nextSpawnTime = getNextAsteroidSpawnTime();
         }
+
+        if(timer > nextEnemySpawnTime) {
+            spawnEnemy();
+            nextEnemySpawnTime = getNextEnemySpawnTime();
+        }
+        
+        timer+=Time.deltaTime;
     }
 
     private void spawnAsteroid() {
@@ -40,16 +47,31 @@ public class AsteroidSpawerScript : MonoBehaviour
         Instantiate(asteroids[randomIndex], new Vector3(Random.Range(leftestPoint, rightestPoint), transform.position.y, 0), transform.rotation);
     }
 
-    private float getNextSpawnTime() {
+    private float getNextAsteroidSpawnTime() {
         spawnSpeed = System.Math.Max(spawnSpeed - spawnSpeedAcceleration, 0);
         spawnSpeedAcceleration += .002f;
-        float output = System.Math.Max(spawnSpeed + Random.Range(spawnRandomness*-1, spawnRandomness), minSpawnSpeed);
+        float output = timer + System.Math.Max(spawnSpeed + Random.Range(spawnRandomness*-1, spawnRandomness), minSpawnSpeed);
 
         // Debug.Log(
         //     "spawnSpeed: " + spawnSpeed
         //     + " spawnSpeedAcceleration: " + spawnSpeedAcceleration
         //     + " nextSpawnTime: " + output
         // );
+
+        return output;
+    }
+
+    private void spawnEnemy() {
+        int randomIndex = Random.Range(0, enemies.Length);
+        float leftestPoint = transform.position.x - horizontalOffset;
+        float rightestPoint = transform.position.x + horizontalOffset;
+
+        Instantiate(enemies[randomIndex], new Vector3(Random.Range(leftestPoint, rightestPoint), transform.position.y, 0), transform.rotation);
+    }
+
+    private float getNextEnemySpawnTime() {
+        float enemySpawnSpeed = ((timer-20)*.001f * -1) + 5f;
+        float output = timer + System.Math.Max(enemySpawnSpeed + Random.Range(spawnRandomness*-1, spawnRandomness), minSpawnSpeed);
 
         return output;
     }
