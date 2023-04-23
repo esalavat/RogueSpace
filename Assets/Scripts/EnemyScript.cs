d97109bb181d8317393cb17e84040333338bb730
esalavat <eric.salavat@gmail.com>
2023-04-23 15:44:40 -0700

    Added explosions

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,7 @@ public class EnemyScript : MonoBehaviour {
     public int scoreValue = 1;
     public float coinChance = .33f;
     public GameObject coin;
+    public GameObject explosion;
 
     void OnTriggerEnter2D(Collider2D collider) {
         if(collider.gameObject.tag == "laser") {
@@ -11,6 +12,7 @@ public class EnemyScript : MonoBehaviour {
             float y = transform.position.y;
             Destroy(gameObject);
             SpawnCoin(x, y);
+            SpawnExplosion(x, y);
             EventManager.EnemyDestroyed(scoreValue);
         }
     }
@@ -19,6 +21,13 @@ public class EnemyScript : MonoBehaviour {
         float rand = Random.value;
         if(rand <= coinChance) {
             Instantiate(coin, new Vector3(x, y, 0), Quaternion.identity);
+        }
+    }
+
+    void SpawnExplosion(float x, float y) {
+        if(explosion != null) {
+            GameObject newExplosion = Instantiate(explosion, new Vector3(x, y, 0), Quaternion.identity);
+            Destroy(newExplosion, 2);
         }
     }
 }
